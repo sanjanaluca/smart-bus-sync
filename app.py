41eafcd0acce3687a9227f224b205a9ea47472e8
Sanjana <sanjanamohan.1224@gmail.com>
2026-06-20 19:46:42 +0530

    add student status features

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from config.db import get_db_connection
+from datetime import date
 
 app = Flask(__name__)
 
@@ -7,35 +8,65 @@ app = Flask(__name__)
 def home():
 
     student = None
+    message = ""
 
     if request.method == 'POST':
 
-        student_id = request.form['student_id']
+        action = request.form['action']
 
-        conn = get_db_connection()
-        cur = conn.cursor()
+        if action == "search":
 
-        cur.execute("""
-            SELECT
-                s.id_number,
-                s.student_name,
-                s.college_name,
-                s.department,
-                s.year,
-                s.bus_number,
-                bs.stop_name
-            FROM student s
-            JOIN bus_stop bs
-            ON s.stop_id = bs.stop_id
-            WHERE s.id_number = %s
-        """, (student_id,))
+            student_id = request.form['student_id']
 
-        student = cur.fetchone()
+            conn = get_db_connection()
+            cur = conn.cursor()
 
-        cur.close()
-        conn.close()
+            cur.execute("""
+                SELECT
+                    s.id_number,
+                    s.student_name,
+                    s.college_name,
+                    s.department,
+                    s.year,
+                    s.bus_number,
+                    bs.stop_name
+                FROM student s
+                JOIN bus_stop bs
+                ON s.stop_id = bs.stop_id
+                WHERE s.id_number = %s
+            """, (student_id,))
 
-    return render_template('index.html', student=student)
+            student = cur.fetchone()
+
+            cur.close()
+            conn.close()
+
+        elif action == "save":
+
+            student_id = request.form['student_id']
+            status = request.form['status']
+
+            conn = get_db_connection()
+            cur = conn.cursor()
+
+            cur.execute("""
+                INSERT INTO student_status
+                (student_id, status, status_date)
+                VALUES (%s, %s, %s)
+            """, (student_id, status, date.today()))
+
+            conn.commit()
+
+            cur.close()
+            conn.close()
+
+            message = "Status saved successfully"
+
+    return render_template(
+        'index.html',
+        student=student,
+        message=message
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
